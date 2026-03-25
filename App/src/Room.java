6df12426b0762949761e4992b3e08bcef21b2fff
sunishka999-a11y <sunishka999@gmail.com>
2026-03-25 09:13:16 +0530

    sunishka999-a11y ADDED UC6

---
+++ App/src/Room.java
@@ -0,0 +1,34 @@
+public abstract class Room {
+
+    /** Number of beds available in the room */
+    protected int numberOfBeds;
+
+    /** Total size of the room in square feet */
+    protected int squareFeet;
+
+    /** Price charged per night for this room type */
+    protected double pricePerNight;
+
+    /**
+     * Constructor used by child classes
+     * to initialize common room attributes.
+     *
+     * @param numberOfBeds number of beds in the room
+     * @param squareFeet total room size
+     * @param pricePerNight cost per night
+     */
+    public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
+        this.numberOfBeds = numberOfBeds;
+        this.squareFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
+    }
+
+    /**
+     * Displays room details.
+     */
+    public void displayRoomDetails() {
+        System.out.println("Beds: " + numberOfBeds);
+        System.out.println("Size: " + squareFeet + " sqft");
+        System.out.println("Price per night: " + pricePerNight);
+    }
+}
